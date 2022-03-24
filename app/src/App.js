c912bbc81d28c977d03b84c1d86bca418f7063a1
Ryan DeGroot <degrootr253@gmail.com>
2022-03-24 11:01:33 -0700

    Navbar design and functioning

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <h1 className="text-16xl font-bold">Hello</h1>
+      <Navbar/>
     </div>
   );
 }
