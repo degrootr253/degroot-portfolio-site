abf52d15bea460af6a97a3cf45e04cf9f81bfdc2
Ryan DeGroot <degrootr253@gmail.com>
2022-03-28 19:56:58 -0700

    updating about component

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
                 </div>
                 <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
                     <div className='sm:text-right text-4xl font-bold'>
-                        <p>Hi, I'm my name is Ryan, please take a look around!</p>
+                        <p>Hi, my name is Ryan, please take a look around!</p>
                     </div>
                     <div>
                         <p>
